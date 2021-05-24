083b14426273a0aaee6d7c344b73eedec9d38f43
DEMOSHEEREEGOCHI <71782903+DEMOSHEEREEGOCHI@users.noreply.github.com>
2021-05-25 01:34:55 +0300

    db prototype [pre-alpha]

---
+++ index.js
@@ -0,0 +1,81 @@
+import * as db from 'db_connect.js';
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { response } = require('express');
+
+const app = express();
+app.use(cors());
+
+const asyncHandler = (fn) => (req, res, next) => {
+    try {
+        fn(req, res, next)
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+
+
+app.use((req, res, next) => {
+
+
+    console.log('URL = ' + req.url);
+    console.log('METHOD = ' + req.method);
+    console.log('HOST = ' + req.headers.host);
+    console.log('IsSecure =' + req.secure);
+    console.log('Query = ' + req.query);
+    console.log('Body = ' + req.body);
+
+    next();
+})
+
+
+app.get('/me', (req, res) => {
+    res.status(200).json({ message: 'All is ok!' });
+})
+
+let todoList = [];
+var todoCount = 0;
+
+function upToDo(index) {
+    let todo = {
+        id: index,
+        title: 'Title =' + index,
+        descr: `Descr ${index}`
+    }
+    todoList.push(todo);
+    todoCount += 1;
+}
+
+function deleteToDo(index) {
+    todoList.splice(index);
+    todoCount -= 1;
+}
+
+for (let index = 0; index < 10; index++) {
+    upToDo(index);
+}
+
+app.get(`/todoList`, (req, res) => {
+    res.status(200).json({ todoList });
+})
+
+app.post('/addtodo', (req, res) => {
+    upToDo(todoCount);
+    res.status(200).json("add to do success");
+})
+app.delete('/deletetodo', (req, res) => {
+    deleteToDo(todoCount - 1);
+    res.status(200).json("delete to do success");
+})
+
+
+http.createServer(app).listen(3000, () => {
+    console.log('Server is working on port 3000');
+    console.log(todoCount);
+})
+
+db.connect;
+db.end;
